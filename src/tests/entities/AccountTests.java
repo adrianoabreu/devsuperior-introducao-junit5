546253a70b5018782d11a6944e8d55f0826e7e0e
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-06-08 19:21:09 -0300

    Primeiro teste com JUnit 5

---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,30 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+//Classe que implementa testes com JUnit 5.
+public class AccountTests {
+	
+	//Nomenclatura de um teste
+	// <AÇÃO> should <EFEITO> [when <CENÁRIO>] 
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		//Padrão AAA(Arrange, Act, Assert)
+		
+		//Arrange
+		Double amount        = 200.00;
+		Double expectedValue = 196.00;
+		Account acc          = new Account(1L, 0.0);
+		
+		//Act
+		acc.deposit(amount);
+		
+		//Assert
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+		
+	}
+	
+}
